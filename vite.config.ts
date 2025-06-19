702bdf0084df207f0daddee25b25363c3ed70d74
CynthiaBetsy <betsycynthia@gmail.com>
2025-06-19 16:58:29 +0100

    Add index.html package-lock.json vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,20 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss(),
+//   ],
+//   base: './',
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
-theme: {
-  extend: {
-    animation: {
-      fadeIn: 'fadeIn 0.3s ease-out',
-    },
-    keyframes: {
-      fadeIn: {
-        '0%': { opacity: 0 },
-        '100%': { opacity: 1 },
-      },
-    },
-  },
-},
-
-  
+  plugins: [react()],
+  base: './',
 })
